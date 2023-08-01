a61c347b87642792e667c42cf1359d3b7102b886
ultim <ultimate.data.fill@gmail.com>
2023-08-01 18:51:41 +0300

    ABC!

--- weather.py
+++ weather.py
@@ -11,11 +11,11 @@ API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&lang=ua
 FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast/daily?cnt=7&units=metric&lang=ua"
 
 class WeatherCard(MDCard):
-    super().__init__()
     def __init__(self, temp, weather_text, winf_speed, rain, rain_pop=None):
+        super().__init__()
         self.ids.weather_text.text = weather_text.capitalize()
         self.ids.temp_text.text = str(round(temp)) + "C°"
-        if rein_pop:
+        if rain_pop:
             self.ids.rein_pop_text.text = f"Ймовірність опадів: {round(rain_pop)}%"
         self.ids.rein_text.text = f"Кількість опадів: {rain} ММ"
         self.ids.wind_speed_text.text = f"Швидкісь вітру: {rain} М/C"
@@ -38,7 +38,7 @@ class HomeScreen(MDScreen):
         desc_data = response['weather'][0]['description']
         wind_data = response['wind']['speed']
         rain_data = response['rain']['1h']
-        new_card = Weather_card(temp_data, weather_data, wind_data, rain_data)
+        new_card = WeatherCard(temp_data, weather_data, wind_data, rain_data)
         self.ids.weather_carousel.add_widget(new_card)
         print(temp_data)
         print(response)
